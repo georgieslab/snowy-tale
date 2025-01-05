fed5bad1ff4d8d43300d1d445451ee547318f380
georgieslab <georgieslab@gmail.com>
2025-01-05 18:02:35 +0100

    Add deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: '/snowy-tale/', 
+  base: '/snowy-tale/',
   plugins: [react()]
 })
